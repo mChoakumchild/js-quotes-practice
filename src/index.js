4da3a512f23de90492288812a16fefecd23ad6c6
Elijah Schiltz-Rouse <elijahforest101@gmail.com>
2021-04-15 21:36:43 -0400

    First Draft

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ let quoteForm = document.querySelector("form#new-quote-form")
 let submitButton = document.querySelector("button.btn")
 let quoteList = document.querySelector("ul#quote-list")
 let dateNum = Math.floor(Date.now() / 1000)
+let likeArr;
+
 
 document.addEventListener('DOMContentLoaded', (e) => {
     e.preventDefault();
@@ -12,24 +14,33 @@ document.addEventListener('DOMContentLoaded', (e) => {
 //with new dom elements
     quoteForm.addEventListener('submit', (submitEvent) => {
         submitEvent.preventDefault();
-        //console.log('hello')
+        
         
         fetch('http://localhost:3000/quotes?_embed=likes')
         .then(res => res.json())
         .then(res =>{
-            res.forEach( obj => {
+            res.forEach(obj => {
                 if (authorInput.value == obj.author){
-                    console.log(obj)
+                    console.log('hello')
                     quoteFunction(obj) 
-
                 }
             })
         })
     })      
-  function likedef (nLikes = 0) {
-            return nLikes
+
+  function likedef (nLikes) {
+      if (typeof nLikes == undefined){
+          nLikes = 0
+      }
+      else{
+        return nLikes
+      }
   }
 
+
+
+
+// This function creates new elements
     function quoteFunction(quoteObject) {
         let quoteInfo = document.createElement('form')
             quoteInfo.className = quoteObject.author
@@ -39,8 +50,10 @@ document.addEventListener('DOMContentLoaded', (e) => {
         let dateTime = document.createElement('li')
         let likeButton = document.createElement('button')
         let deleteButton = document.createElement('button')
-        let nLikes = quoteObject.likes.quoteId
-            nLikes = likedef(nLikes)
+        let editButton = document.createElement('button')
+        let nLikes = quoteObject.likes.likesquoteId
+        console.log(quoteObject.likes.quoteId)
+        nLikes = likedef(nLikes)
         quoteString.innerText = quoteObject.quote
         authorString.innerText = quoteObject.author
         authorString.className = "author"
@@ -50,51 +63,67 @@ document.addEventListener('DOMContentLoaded', (e) => {
         dateTime.className = "date"
         likeButton.innerText = "Like"
         deleteButton.innerText = "Delete"
+        deleteButton.innerText = "Edit"
 
-        quoteInfo.append(quoteString, authorString, likeString, dateTime, likeButton, deleteButton)
+        quoteInfo.append(quoteString, authorString, likeString, dateTime, likeButton, deleteButton, editButton)
         quoteList.append(quoteInfo)
-
+// this makes those elements editable
     quoteInfo.addEventListener("click", (e)=> {
         e.preventDefault();
             if (e.target == likeButton){
-                //console.log("hahaha")
+                console.log(quoteObject.id)
                
-                POSTverb(quoteObject, likeString, dateTime, nLikes)
-                
+                fetch(`http://localhost:3000/likes?quoteId=${quoteObject.id}`)
+                .then(res => res.json())
+                .then(res => {
+                    likeArr = res.quoteId 
+                })
+                nLikes = likeArr        
+                //let dateNum = Math.floor(Date.now() / 1000)
+                fetch(`http://localhost:3000/likes?quoteId=${quoteObject.id}`,
+                 {
+                     method: "POST",
+                     headers: {
+             "Content-Type" : "application/json"
+                             },
+             body: JSON.stringify({
+             quoteId: nLikes + 1,
+             createdAt: dateNum
+                                 })
+                 })
+                 .then(res => res.json())
+                 .then(res =>{
+                    nLikes += 1
+                     likeString.innerText = `Likes: ${res.quoteId}`
+                     dateTime.innerText = res.createdAt
+                     quoteObject.likes.quoteId = res.quoteId
+                     quoteObject.likes.createdAt = dateNum
+                 })
+            }
+            else if (e.target == editButton) {
+                fetch(`http://localhost:3000/quotes/${quoteObject.id}`,
+                {
+                    method: "PATCH",
+                    headers: {
+            "Content-Type" : "application/json"
+                            },
+            body: JSON.stringify({
+            quote: quoteInput.value ,
+            author: authorInput.value
+                                })
+                })
             }
             else{
                 e.target.parentNode.remove()
-                //e.target.parentNode.remove()
+                console.log(quoteObject)
+                //e.target.innerText = ""
                 //console.log(document.querySelector(`form#${quoteObject.author}`))
             }
         })
     } 
 //post
 
- function POSTverb(quoteObject, likeString, dateTime, nLikes) { 
-    let dateNum = Math.floor(Date.now() / 1000)
-       return fetch(`http://localhost:3000/quotes/${quoteObject.id}/likes`,
-        {
-            method: "POST",
-            headers: {
-    "Content-Type" : "application/json"
-                    },
-    body: JSON.stringify({
-    "quoteId": nLikes + 1,
-    "createdAt": dateNum
-                        })
-        })
-        .then(res => res.json())
-        .then(res =>{
-            console.log("hahaha")
-            // Update the DOM
-            likeString.innerText = `Likes: ${res.quoteId}`
-            dateTime.innerText = res.createdAt
-            //update object in mem
-            quoteObject.likes.quoteId += 1
-            quoteObject.likes.createdAt = dateNum
-        })
-}
+ 
 
 
 
